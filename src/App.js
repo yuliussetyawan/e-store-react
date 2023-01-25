f29fc1bf23c266287b1466d7c9d2c59d5ee3066d
yuliussetyawan <yuliusetyawan@gmail.com>
2023-01-25 23:54:39 +0700

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 // json-server --watch src/db/db.json --port 3001
 
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResult(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {result.map((d) => (
+        <div key={d.id}>{d.title}</div>
+      ))}
     </div>
   );
 }
