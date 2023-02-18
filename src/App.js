ff1cd070682a4f22a0ce9d2ae0e3ed78f19094db
yuliussetyawan <yuliusetyawan@gmail.com>
2023-02-18 23:17:07 +0700

    rendering the categories

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@
 
 import "./App.css";
 import React, { useState } from "react";
+import Category from "./components/Category";
 
 function App() {
-  const [result, setResult] = useState([]);
+  const [results, setResults] = useState([]);
 
   React.useEffect(() => {
     fetch("http://localhost:3001/categories")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setResult(data);
+        setResults(data);
       });
   }, []);
+
+  const renderCategories = () => {
+    const categories = [];
+    for (let i = 0; i < results.length; i++) {
+      categories.push(
+        <Category
+          key={results[i].id}
+          id={results[i].id}
+          title={results[i].title}
+        />
+      );
+    }
+    return categories;
+  };
 
   return (
     <>
       <header>My Store</header>
       <section>
         <nav>
-          {result.map((d) => (
+          {results.map((d) => (
             <div key={d.id}>{d.title}</div>
           ))}
         </nav>
---
+++ src/components/Category.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Category = ({id,title}) => {
+  return <div key={id}>{title}</div>;
+};
+
+export default Category;
