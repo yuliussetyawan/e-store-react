05698ea195a3f6e8418711fd80c62eea216f3aa1
yuliussetyawan <yuliusetyawan@gmail.com>
2023-02-21 23:41:16 +0700

    Dealing with errors in Fetch API

--- src/App.js
+++ src/App.js
@@ -3,32 +3,31 @@
 import "./App.css";
 import React, { useState } from "react";
 import Category from "./components/Category";
-import { fetcher } from "./fetcher";
+import {getCategories, getProducts} from "./fetcher";
 
 function App() {
-  const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([{errorMessage:' ',data:[]}]);
+  const [products, setProducts] = useState([{errorMessage:' ',data:[]}]);
 
   React.useEffect(() => {
     const fetchData = async() => {
-      const data = await fetcher("/categories");
-      setCategories(data);
+      const responseObject = await getCategories();
+      setCategories(responseObject);
     };
     fetchData();
     
   }, []);
 
   const handleCategoryClick = (id) => {
-    fetch("http://localhost:3001/products?catId=" + id)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setProducts(data);
-      });
+    const fetchData = async() => {
+      const responseObject = await getProducts(id);
+      setProducts(responseObject);
+    };
+    fetchData();
   };
 
   const renderCategories = () => {
-    return categories.map((c) => (
+    return categories.data.map((c) => (
       <Category
         key={c.id}
         id={c.id}
@@ -39,15 +38,19 @@ function App() {
   };
 
   const renderProducts = () => {
-    return products.map((p) => <div>{p.title}</div>);
+    // https://stackoverflow.com/questions/69080597/%C3%97-typeerror-cannot-read-properties-of-undefined-reading-map
+    return products.data?.map((p) => <div>{p.title}</div>);
   };
 
   return (
     <>
       <header>My Store</header>
       <section>
-        <nav>{categories && renderCategories()}</nav>
+        <nav>
+        {categories.errorMessage && <div>Error: {categories.errorMessage}</div>}
+        {categories.data && renderCategories()}</nav>
         <article>
+        {products.errorMessage && <div>Error: {products.errorMessage}</div>}
           <h1>Products</h1>
           {products && renderProducts()}
         </article>
--- src/fetcher.js
+++ src/fetcher.js
@@ -1,8 +1,23 @@
 const BASE_URL = "http://localhost:3001";
 
 export const fetcher = async (url) => {
+  let responseObject = { errorMessage: "", data: [] };
+  try {
     const response = await fetch(BASE_URL + url);
     const responseData = await response.json();
-    return responseData
-
+    responseObject.errorMessage = "";
+    responseObject.data = responseData;
+  } catch (err) {
+    responseObject.errorMessage = err.message;
+  }
+  return responseObject;
 };
+
+
+export const getCategories = () => {
+    return fetcher('/categories')
+}
+
+export const getProducts = (id) => {
+    return fetcher("/products?catId=" + id)
+}
